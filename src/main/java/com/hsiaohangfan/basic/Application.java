73005bf62a12a9652efa809e49ef6af8bb4448a0
swift <aswift@126.com>
2020-03-22 23:26:39 +0800

    Maven初始化springboot基础项目

---
+++ src/main/java/com/hsiaohangfan/basic/Application.java
@@ -0,0 +1,15 @@
+package com.hsiaohangfan.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * create by hsiaohangfan
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
